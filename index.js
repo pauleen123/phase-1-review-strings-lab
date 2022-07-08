833f93e3f6bb366b4cf1b53c26f877a0c8273bee
pau <paugitonga6@gmail.com>
2022-07-08 12:30:05 +0300

    commit tree f2a54cf1898b7403137a147f2fe269595f39f7c7

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Write your code in this file!
-const currentUser = 'Grace Hopper';
+const currentUser = 'Pauleen Muthoni';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
